0d855508ff960e5bd3903e89cde7c716415cca28
lilinxi015 <63709448+lilinxi015@users.noreply.github.com>
2020-07-13 21:25:13 +0800

    1.0
    v1.0

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+// 作者：lilinxi email:lixingui015@163.com
+<template>
+  <div id="app">
+    <!-- 路由占位符 -->
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+</style>
